219271096de886cf127769add965ab3d5d82a4ef
wznmickey <first@wznmickey.com>
2025-01-19 13:24:08 -0500

    add i18n

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
 #[macro_export]
 macro_rules! error {
     ($fmt:expr, $($args:tt)*) => {
-        log::log!(log::Level::Error, "{}", t!($fmt, $($args)*));
+        log::log!(log::Level::Error, "{}", rust_i18n::t!($fmt, $($args)*));
     };
 }
 
 #[macro_export]
 macro_rules! warn {
     ($fmt:expr, $($args:tt)*) => {
-        log::log!(log::Level::Warn, "{}", t!($fmt, $($args)*));
+        log::log!(log::Level::Warn, "{}", rust_i18n::t!($fmt, $($args)*));
     };
 }
 
 #[macro_export]
 macro_rules! info {
     ($fmt:expr, $($args:tt)*) => {
-        log::log!(log::Level::Info, "{}", t!($fmt, $($args)*));
+        log::log!(log::Level::Info, "{}", rust_i18n::t!($fmt, $($args)*));
     };
 }
 
 #[macro_export]
 macro_rules! debug {
     ($fmt:expr, $($args:tt)*) => {
-        log::log!(log::Level::Debug, "{}", t!($fmt, $($args)*));
+        log::log!(log::Level::Debug, "{}", rust_i18n::t!($fmt, $($args)*));
     };
 }
 
 #[macro_export]
 macro_rules! trace {
     ($fmt:expr, $($args:tt)*) => {
-        log::log!(log::Level::Trace, "{}", t!($fmt, $($args)*));
+        log::log!(log::Level::Trace, "{}", rust_i18n::t!($fmt, $($args)*));
     };
 }
